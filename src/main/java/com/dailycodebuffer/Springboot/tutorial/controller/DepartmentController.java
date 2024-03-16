15f893acad7797749ac5b9ee551ddeb5eb272783
prateekmohanty63 <prateekmohanty63@gmail.com>
2024-03-16 17:31:01 +0530

    Feat:Department added lombok dependencies

--- src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
@@ -3,6 +3,9 @@ package com.dailycodebuffer.Springboot.tutorial.controller;
 import com.dailycodebuffer.Springboot.tutorial.entity.Department;
 import com.dailycodebuffer.Springboot.tutorial.service.DepartmentService;
 import com.dailycodebuffer.Springboot.tutorial.service.DepartmentServiceImpl;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +16,17 @@ public class DepartmentController {
 
     @Autowired
     private DepartmentService departmentService;
+
+    private final Logger LOGGER= LoggerFactory.getLogger(DepartmentController.class);
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("Inside saveDepartment of DepartmentController");
        return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
